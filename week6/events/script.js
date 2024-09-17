289872f056a970c8dc0665fce6955af53dd6ec97
sherineelk <142776502+sherineelk@users.noreply.github.com>
2024-09-17 14:41:51 +1000

    week 7 classwork

---
+++ week6/events/script.js
@@ -0,0 +1,51 @@
+// const myButton = document.querySelector("#my-button");
+// console.log(myButton);
+
+// myButton.addEventListener("click", myFunction);
+// let clickCount = 0;
+// const count = document.querySelector("#count");
+
+// function myFunction() {
+//   console.log("oh you clicked me!");
+//   clickCount++;
+//   count.textContent = clickCount;
+// }
+
+const boxContainer = document.querySelector(".box-container");
+console.log(boxContainer);
+
+const toggleButton = document.querySelector("#toggle-button");
+console.log(toggleButton);
+toggleButton.addEventListener("click", toggleMe);
+
+function toggleMe() {
+  console.log("togglebutton is clicked");
+  boxContainer.classList.toggle("row-reverse");
+}
+
+const addButton = document.querySelector("#add-button");
+console.log(addButton);
+addButton.addEventListener("click", addMe);
+
+let count = 0;
+function addMe() {
+  console.log("addbutton is pressed");
+  if (count % 2 === 0) {
+    boxContainer.innerHTML += `<div class="box purple-box"></div>`;
+  } else {
+    boxContainer.innerHTML += `<div class="box coral-box"></div>`;
+  }
+  count++;
+}
+const removeButton = document.querySelector("#remove-button");
+console.log(removeButton);
+removeButton.addEventListener("click", removeMe);
+
+function removeMe() {
+  let lastBox = boxContainer.lastElementChild;
+  console.log(lastBox);
+  if (lastBox) {
+    lastBox.remove();
+  }
+  count--;
+}
